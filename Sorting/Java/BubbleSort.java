d17d6e75bda33c896c23c43b94fd15eec420cc7f
Ankush Rodewad <rodewad.ankush29@gmail.com>
2018-05-15 11:21:46 +0530

    Update BubbleSort.java

--- Sorting/Java/BubbleSort.java
+++ Sorting/Java/BubbleSort.java
@@ -28,10 +28,9 @@ class BubbleSort
 	public static void main(String args[])
 	{
 		int arr[] = {10, 7, 8, 9, 1, 5};
-		int n = arr.length;
 
-		QuickSort ob = new QuickSort();
-		ob.sort(arr, 0, n-1);
+		BubbleSort ob = new BubbleSort();
+		ob.sort(arr);
 
 		System.out.println("sorted array");
 		printArray(arr);
