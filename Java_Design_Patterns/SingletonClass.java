faccc44c5e4d18ac266b7192dce7ad8c7432b450
Amr Hesham <amr96@programmer.net>
2018-09-12 23:38:37 +0200

    Fix SingleTon NullPointerException
    Fix NullPointerException on SingletonClass by check if it equal null or not

--- Java_Design_Patterns/SingletonClass.java
+++ Java_Design_Patterns/SingletonClass.java
@@ -1,7 +1,7 @@
 public class SingletonClass 
 {
 	//static instance of SingletonClass
-	private static SingletonClass instance;
+	private static SingletonClass sInstance;
 	//unreachable constructor
 	private SingletonClass() {
 		
@@ -9,6 +9,9 @@ public class SingletonClass
 	//access method to static instance of SingletonClass
 	public static SingletonClass getInstance()
 	{
-		return instance;
+	    if(sInstance == null){
+                sInstance = new SingletonClass();
+            }
+            return sInstance;
 	}
 }
