d940411a7b6f8289fd0c0316dfa74fdd1932e10a
T Thiyagaraj <tstream.h@outlook.com>
2018-10-02 22:14:36 +0530

    Update builder.java

--- Java Design Patterns/builder /builder.java
+++ Java Design Patterns/builder /builder.java
@@ -62,7 +62,7 @@ class Computer {
 public class TestBuilderPattern {
 	public static void main(String[] args) {
 		// Using builder to get the object in a single line of code and 
-        // without any inconsistent state or arguments management issues	
+        	// without any inconsistent state or arguments management issues	
 		Computer comp = new Computer.ComputerBuilder(
 		"500 GB", "2 GB").setBluetoothEnabled(true)
 		.setGraphicsCardEnabled(true).build();
