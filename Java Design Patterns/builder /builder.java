4e2726d431b237e25388063878831bb4d9182bfe
aananya <astream.h@outlook.com>
2018-10-02 20:27:46 +0530

    formatting

--- Java Design Patterns/builder /builder.java
+++ Java Design Patterns/builder /builder.java
@@ -1,6 +1,5 @@
-package com.journaldev.design.builder;
 
-public class Computer {
+class Computer {
 	
 	//required parameters
 	private String HDD;
@@ -69,19 +68,17 @@ public class Computer {
 }
 
 
-package com.journaldev.design.test;
 
-import com.journaldev.design.builder.Computer;
 
 public class TestBuilderPattern {
 
 	public static void main(String[] args) {
-		//Using builder to get the object in a single line of code and 
-        //without any inconsistent state or arguments management issues	
+		// Using builder to get the object in a single line of code and 
+        // without any inconsistent state or arguments management issues	
 
-		//Computer comp = new Computer.ComputerBuilder(
-		//		"500 GB", "2 GB").setBluetoothEnabled(true)
-		//		.setGraphicsCardEnabled(true).build();
+		Computer comp = new Computer.ComputerBuilder(
+				"500 GB", "2 GB").setBluetoothEnabled(true)
+				.setGraphicsCardEnabled(true).build();
 	}
 
 }
