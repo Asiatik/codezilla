a318b0ef5614b82fcedb04ba5ecd1bb779e6d03c
find3rskeeper <njain4u@gmail.com>
2018-10-03 10:27:48 +0530

    added proper directory structure

--- Dynamic Programming/ways_to_decode/C++/dp_ways_to_decode.cpp
+++ Dynamic Programming/ways_to_decode/C++/dp_ways_to_decode.cpp
@@ -1,14 +1,17 @@
-//A message containing letters from A-Z is being encoded to numbers using the following mapping:
-//'A' -> 1
-//'B' -> 2
-//...
-//'Z' -> 26
+/* @author Navneet Jain
+ * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
 
-//Given an encoded message containing digits, determine the total number of ways to decode it.
+ * Given an encoded message containing digits, determine the total number of ways to decode it.
 
-//Example :
-//Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
-//The number of ways decoding "12" is 2.
+ * Example :
+ * Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+ * The number of ways decoding "12" is 2.
+
+ */
 
 #include<iostream>
 #include<vector>
