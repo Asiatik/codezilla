c7597775307cd732d65552c906dd0b6599f8f2e7
Rachit Shukla <21.rachitshukla@gmail.com>
2018-10-03 16:28:29 +0530

    Reformatted

--- Sorting/Bubble Sort/C/BubbleSort.c
+++ Sorting/Bubble Sort/C/BubbleSort.c
@@ -12,11 +12,13 @@ void bubbleSort(int arr[], int n)
 { 
    int i, j; 
    for (i = 0; i < n-1; i++)       
-  
-       // Last i elements are already in place    
-       for (j = 0; j < n-i-1; j++)  
+   {  
+       for (j = 0; j < n-i-1; j++)
+       {
            if (arr[j] > arr[j+1]) 
               swap(&arr[j], &arr[j+1]); 
+       }
+   }
 } 
   
 void printArray(int arr[], int size) 
@@ -24,7 +26,7 @@ void printArray(int arr[], int size)
     int i; 
     for (i=0; i < size; i++) 
         printf("%d ", arr[i]); 
-    printf("n"); 
+    printf("\n"); 
 } 
   
 int main() 
