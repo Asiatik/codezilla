f9e25208ec63e201628afa2be0ff0ed7960d93cc
find3rskeeper <njain4u@gmail.com>
2018-10-04 11:24:55 +0530

    removed references from readme and code

--- Dynamic Programming/buy_sell_stocks/C++/buy_sell_stock_dp.cpp
+++ Dynamic Programming/buy_sell_stocks/C++/buy_sell_stock_dp.cpp
@@ -1,6 +1,4 @@
 /*@author Navneet Jain
- * https://www.interviewbit.com/problems/best-time-to-buy-and-sell-stocks-ii/
-
  * Say you have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
