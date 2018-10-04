4d151bdbfa0a4ae568b641ceb49f64c4bfa39b6d
Prakhar Varshney <prakharnegative@gmail.com>
2018-10-04 23:21:04 +0530

    Added a Function bubble_sort and a test array

--- Sorting/Bubble Sort/Python/bubble.py
+++ Sorting/Bubble Sort/Python/bubble.py
@@ -1,17 +1,16 @@
-arr = list()
-
-abc = int(input("Enter the number of terms"))
-
-for i in range(abc):
-    b = int(input())
-    arr.append(b)
-
-
-size = len(arr)
-for i in range(size):
-    for j in range(0,n-i-1):
-        if a[j]>a[j+1]:
-            a[j],a[j+1] = a[j+1],a[j]
+def bubble_sort(arr):
+    size = len(arr)
+    for i in range(size):
+        for j in range(0,size-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
 
 
-print(arr)
+test_arr = [ 12, 11, 13, 5, 6, 7]
+print("List before sorting")
+print(test_arr)
+bubble_sort(test_arr)
+print("List After Sorting")
+print(test_arr)
+
+#Bubble Sorting by Prakhar Varshney
