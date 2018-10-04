a382b55bb213966ea7881624962d47a09c705689
Prakhar Varshney <prakharnegative@gmail.com>
2018-10-04 23:28:08 +0530

    Update merge_sort.py

--- Sorting/Merge Sort/Python/merge_sort.py
+++ Sorting/Merge Sort/Python/merge_sort.py
@@ -1,46 +1,46 @@
-def merge(arr,l,m,r):
-    n1 = (m-l)  + 1
-    n2 = r-m
+def merge(arr,left,mid,right):
+    n1 = (mid-left)  + 1
+    n2 = right-mid
 
-    L = [0]*(n1)
-    R = [0]*(n2)
+    Left_arr = [0]*(n1)
+    Right_arr = [0]*(n2)
 
     for i in range(0,n1):
-        L[i] = arr[l + i]
+        Left_arr[i] = arr[left + i]
 
     for j in range(0, n2):
-        R[j] = arr[m + 1 +j]
+        Right_arr[j] = arr[mid + 1 +j]
 
     i = 0
     j = 0
-    k = l
+    k = left
 
     while(i < n1 and j < n2):
-        if(L[i]<=R[j]):
-            arr[k] = L[i]
+        if(Left_arr[i]<=Right_arr[j]):
+            arr[k] = Left_arr[i]
             i = i+1
         else:
-            arr[k] = R[j]
+            arr[k] = Right_arr[j]
             j = j+1
         k = k+1
 
     while(i<n1):
-        arr[k] = L[i]
+        arr[k] = Left_arr[i]
         i = i+1
         k = k+1
 
     while(j<n2):
-        arr[k] = R[j]
+        arr[k] = Right_arr[j]
         j = j+1
         k = k+1
 
-def mergeSort(arr,l,r):
-    if(l<r):
-        m = int((l+(r-1))/2)
+def mergeSort(arr,left,right):
+    if(left<right):
+        mid = int((left+(right-1))/2)
 
-        mergeSort(arr,l,m)
-        mergeSort(arr,m+1,r)
-        merge(arr,l,m,r)
+        mergeSort(arr,left,mid)
+        mergeSort(arr,mid+1,right)
+        merge(arr,left,mid,right)
 
 
 arr = []
