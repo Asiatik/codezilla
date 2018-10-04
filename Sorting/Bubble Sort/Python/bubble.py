e1fbaa7378a40dbef497016f2139cbe10d4fd975
Prakhar Varshney <prakharnegative@gmail.com>
2018-10-04 22:54:00 +0530

    Update bubble.py

--- Sorting/Bubble Sort/Python/bubble.py
+++ Sorting/Bubble Sort/Python/bubble.py
@@ -1,17 +1,17 @@
-a = []
+arr = list()
 
 abc = int(input("Enter the number of terms"))
 
 for i in range(abc):
     b = int(input())
-    a.append(b)
+    arr.append(b)
 
 
-n = len(a)
-for i in range(n):
+size = len(arr)
+for i in range(size):
     for j in range(0,n-i-1):
-        if(a[j]>a[j+1]):
+        if a[j]>a[j+1]:
             a[j],a[j+1] = a[j+1],a[j]
 
 
-print(a)
+print(arr)
