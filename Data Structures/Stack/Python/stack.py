b43cd14dac1e3cc491238df157924c3bdd24081a
asterisk30 <aston30martin@gmail.com>
2018-10-05 00:34:02 +0530

    stray comments and code removed

--- Data Structures/Stack/Python/stack.py
+++ Data Structures/Stack/Python/stack.py
@@ -3,41 +3,39 @@
  * @author asterisk30
  */
 '''
-from os import system
 class Stack:
     def __init__(self):
-        self.items = [] #creates a stack using lists in this program
+        self.items = [] 
  
     def is_empty(self):
-        return self.items == [] #checks if stack is empty 
+        return self.items == [] 
     def push(self, data):
-        self.items.append(data) #pushes element to the "so called" top
+        self.items.append(data) 
  
     def pop(self):
-        return self.items.pop() #pops element from the top
+        return self.items.pop() 
  
     def disp(self):
-            print(self.items,end=" ")#displays the stack contents
+            print(self.items,end=" ")
  
-#driver program` 
-s = Stack()
-while True:
-    #system('clear') 
-    if s.is_empty():
-        print("EMPTY : ",end = " ")
-    s.disp()
-    print('\npush <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
- 
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        s.push((do[1]))
-    elif operation == 'pop':
+#driver program
+if __name__=='__main__':
+    s = Stack()
+    while True:
         if s.is_empty():
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', s.pop())
-    elif operation == 'quit':
-        break
+            print("EMPTY : ",end = " ")
+        s.disp()
+        print('\npush <value>')
+        print('pop')
+        print('quit')
+        do = input('What would you like to do? ').split()
+        operation = do[0].strip().lower()
+        if operation == 'push':
+            s.push((do[1]))
+        elif operation == 'pop':
+            if s.is_empty():
+                print('Stack is empty.')
+            else:
+                print('Popped value: ', s.pop())
+        elif operation == 'quit':
+            break
