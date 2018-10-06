3031bb0676c1b0f559533d04f203aa523c56fe7b
vishad2 <31646315+vishad2@users.noreply.github.com>
2018-10-06 17:26:31 +0530

    Update SimpleQueue.cpp
    Queue implementation using class

--- Data Structures/Queue/C++/StandardQueue/SimpleQueue.cpp
+++ Data Structures/Queue/C++/StandardQueue/SimpleQueue.cpp
@@ -1,97 +1,105 @@
-#include "SimpleQueue.hpp"
+#include<iostream.h>
+#include<conio.h>
+class queue
+{
+	private:
+		int arr[10];
+		int r,f;
+	public:
+		queue()
+		{
+			f=r=-1 ;
+		}
+	void enqueue(int a);
+	int dequeue();
+	void display();
+};
+void queue::enqueue(int a)
+{
+	cout<<a;
+	if(f==-1)
+	{
+		f=r=0;
+		arr[r]=a;
+		cout<<arr[r];
+	}
+	else
+	{
+	  r++;
+	  arr[r]=a;
+	  cout<<arr[r];
 
-/*
-* SimpleQueue class constructor
-* @params length of the queue
-*/
-template<typename T>
-SimpleQueue<T>::SimpleQueue(int size){
-	this->head_ptr = (T*) calloc(size,sizeof(T));
-	this->tail = this->head_ptr - 1;
-	this->size = size;
-}
-
-
-/*
-* Return true if queue is empty or false,otherwise.
-* Complexity : O(1)
-* @return bool
-*/
-template<typename T>
-bool SimpleQueue<T>::isEmpty(){
-	return (this->tail < this->head_ptr);
-}
-
-
-/*
-* Return true if queue is full or false,otherwise.
-* Complexity : O(1)
-* @return bool
-*/
-template <typename T>
-bool SimpleQueue<T>::isFull(){
-	return (this->tail == (this->head_ptr + this->size - 1));
-}
-
-
-/*
-* Insert a new element in tail of the queue or throw an exception if queue is full.
-* Complexity : O(1)
-* @param element
-* @throw QueueOverflowException
-*/
-template <typename T>
-void SimpleQueue<T>::enqueue(T element){
-	if(!this->isFull()){
-		this->tail++;
-		*this->tail = element;
-	}else{
-		throw this->overflow;	
 	}
 }
-
-
-/*
-* Remove head element of the queue or throw an exception if queue is empty
-* Complexity : O(n)
-* @return element
-* @throw QueueUnderflowException
-*/
-template <typename T>
-T* SimpleQueue<T>::dequeue(){
-	if(!this->isEmpty()){
-		T removedElement = *(this->head_ptr);
-		this->shiftLeft();
-		this->tail--;
-		this->tail[1] = removedElement;
-		return &this->tail[1];
-	}else{
-		throw this->underflow;
+int queue::dequeue()
+{
+	int x;
+	if(f==-1)
+	{
+		cout<<"Nothing to delete";
+		return -999;
+	}
+	else if(f==r)
+	{
+		x=f;
+		r=f=-1;
+	}
+	else
+	{
+	    x=f;
+	    f++;
+	}
+	return arr[x];
+}
+void queue::display()
+{
+	if(f==-1)
+	cout<<"Nothing to display";
+	else
+	{
+		int i=f;
+		while(i<r+1)
+		{
+			cout<<arr[i]<<"  ";
+			i++;
+		}
 	}
 }
+void main()
+{
+	clrscr();
+	int size;
+	queue ob;
+	char ans='y';
+	while(ans=='y'||ans=='Y')
+	{
+	clrscr();
+	cout<<"\n1.To insert an element into queue.\n2.To remove an element from queue"
+		"\n3.To display the elements of Stack"
+		"\n\nEnter your choice\n";
+	cin>>size;
+	switch(size)
+	{
+		case 1:
+			int a;
+			cout<<"\nEnter the element to be insert to queue\n";
+			cin>>a;
+			ob.enqueue(a);
+			break;
+		case 2:
+			a=ob.dequeue();
+			if(a!=-999)
+			cout<<"\n"<<a<<" is removed\n";
+			break;
 
-
-/*
-* When an element is removed, this method move each element to the previous position.
-*/
-template<typename T>
-void SimpleQueue<T>::shiftLeft(){
-	for(int *i = this->head_ptr; i < this->tail;i++){
-		*i = *(i + 1);
+		case 3:
+			ob.display();
+			break;
+		default:
+			cout<<"\nWrong choice!!!!!!\n";
 	}
-}
-
-
-/*
-* Return element in the head of the queue without remove, or NULL if queue is empty.
-* Complexity : O(1)
-* @return element
-*/
-template<typename T>
-T* SimpleQueue<T>::head(){
-	if(!this->isEmpty()){
-		return this->head_ptr;
-	}else{
-		return NULL;
+	cout<<"\nEnter 'y' to continue else 'n' to quit\n";
+	cin>>ans;
 	}
+	getch();
 }
