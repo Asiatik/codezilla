8700bc1de1e9d24dfbda408e1cd7f1b2e27e38a8
keshriraj7870 <43811164+keshriraj7870@users.noreply.github.com>
2018-10-07 01:55:59 +0530

    updation complete

--- Searching/Linear Search/C/LinearSearch.c
+++ Searching/Linear Search/C/LinearSearch.c
@@ -19,6 +19,7 @@ int main(int argc,char* argv[])
 {   
     int arr[] = {1,2,3,4,5};
     printf("%d\n",linearSearch(arr,sizeof(arr)/sizeof(arr[0]),3));
+    scanf("%d",&arr);
     printf("%d",linearSearch(arr,sizeof(arr)/sizeof(arr[0]),0));
     
 }
