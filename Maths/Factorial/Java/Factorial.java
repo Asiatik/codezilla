ee1dd856e74f940a3732bea30441bb5d558ec4c9
ShubhangiShruti <43612947+ShubhangiShruti@users.noreply.github.com>
2018-10-07 00:36:25 +0530

    To get the factorial of a number entered at runtime by the user
    The user can get the factorial of any number by entering it at runtime itself.
    Proper and clear presentation of code is done.

--- Maths/Factorial/Java/Factorial.java
+++ Maths/Factorial/Java/Factorial.java
@@ -1,4 +1,6 @@
-public class Factorial {
+import java.util.Scanner;
+public class Factorial 
+{
 	public static long factorial(int n) {
 		if(n == 1) {
 			return 1;
@@ -7,7 +9,13 @@ public class Factorial {
 			return n * factorial(n-1);
 		}
 	}
-	public static void main(String [] argv) {
-		System.out.println(factorial(5));
+	public static void main(String [] argv) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		System.out.println("Enter the number to get its factorial: ");
+		n=sc.nextInt();
+		double fact=factorial(n));
+		System.out.println("Factorial of "+n+" = "+fact);
 	}
 }
