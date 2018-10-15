1c0959135a016e08c2dd911da3c571dfbab09a5b
villan16 <22690855+physicS16@users.noreply.github.com>
2018-10-15 17:56:42 -0500

    Add C# factorial algorithm

---
+++ Maths/Factorial/C#/Factorial.cs
@@ -0,0 +1,14 @@
+﻿    class Factorial
+    {
+        public static long factorial(int inputNum)
+        {
+            if (inputNum == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return inputNum * factorial(inputNum - 1);
+            }
+        }
+    }
