b0d24738a660cd3409a2520ee41828532d61abb2
Dhruv Aggarwal <dhruvsisd@gmail.com>
2018-10-19 22:39:20 +0530

    Update BucketSort.cpp

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -2,37 +2,40 @@
 #include <algorithm> 
 #include <vector> 
 using namespace std; 
-  
-// Function to sort arr[] of size n using bucket sort 
-void bucketSort(float arr[], int n) 
-{ 
-    // 1) Create n empty buckets 
-    vector<float> b[n]; 
-     
-    // 2) Put array elements in different buckets 
-    for (int i=0; i<n; i++) 
-    { 
-       int bi = n*arr[i]; // Index in bucket 
-       b[bi].push_back(arr[i]); 
-    } 
-  
-    // 3) Sort individual buckets 
-    for (int i=0; i<n; i++) 
-       sort(b[i].begin(), b[i].end()); 
-  
-    // 4) Concatenate all buckets into arr[] 
-    int index = 0; 
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < b[i].size(); j++) 
-          arr[index++] = b[i][j]; 
-} 
-  
+static void BucketSort(int* data, int count) {
+	int minValue = data[0];
+	int maxValue = data[0];
+	for (int i = 1; i < count; i++)
+	{
+		if (data[i] > maxValue)
+			maxValue = data[i];
+		if (data[i] < minValue)
+			minValue = data[i];
+	}
+	int bucketLength = maxValue - minValue + 1;
+	vector<int>* bucket = new vector<int>[bucketLength];
+  for (int i = 0; i < bucketLength; i++)
+	{
+		bucket[i] = vector<int>();}
+for (int i = 0; i < count; i++)
+	{
+		bucket[data[i] - minValue].push_back(data[i]);}
+  int k = 0;
+	for (int i = 0; i < bucketLength; i++)
+	{
+		int bucketSize = bucket[i].size();
+		if (bucketSize > 0)
+		{
+			for (int j = 0; j < bucketSize; j++)
+			{
+				data[k] = bucket[i][j];
+				k++;
+			}}}}
 int main() 
 { 
-    float arr[] = {0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434}; 
+    float arr[] = {0.708, 0.455, 0.025, 0.9899, 0.321, 0.758}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
-    bucketSort(arr, n); 
-  
+    bucketSort(arr, n);  
     cout << "Sorted array is \n"; 
     for (int i=0; i<n; i++) 
        cout << arr[i] << " "; 
