15e14001ccc314664ccd703d894dd3c6bd6ec129
Rohit Agarwal <Rohit@example.com>
2018-10-30 14:32:23 +0530

    tree

--- Trees/AVL.cpp
+++ Trees/AVL.cpp
@@ -10,58 +10,6 @@ int main()
 {
 	AVL < int , int  >   obj;
 	int n,a;
-	// cin>>n;
-	// for(int i=0;i<n;i++)
-	// {
-	// 	cin>>k>>o;
-	// 	obj.insertion(k,o);
-	// }
-	// // obj.insertion(20,23);
-	// // obj.insertion(30,213);
-	// // obj.insertion(25,23);
-	// // obj.insertion(26,23);
-	// // obj.insertion(27,23);
-	// //     obj.insertion(35,23);
-	//   //  obj.insertion(16,23);
-	//   //    obj.insertion(17,23);
-	//   // //  obj.print_post_order() ;
-	//   //    obj.insertion(18,23);
-	//   //    obj.insertion(19,23);
-	//   //   obj.insertion(24,23);
-	//   //  obj.insertion(27,23);
-	// cout<<endl<<endl ;
-	// obj.print_post_order() ;
-	// cout<<endl<<endl;
-	// obj.print_in_order();
-	// cout<<endl<<endl;
-	// obj.print_pre_order();
-
-	// cout<<endl<<"minimum element is " <<obj.minimum()<<endl;
-	// cout<<endl<<"maximum element is "<<obj.maximum()<<endl;
-	// cout<<endl<<"succcessor of "<<k<<" is "<<obj.successor(k)<<endl;
-	// cout<<endl<<"height of tree is "<<obj.getHeight()<<endl;
-	// cout<<endl<<"size of tree is "<<obj.size()<<endl;
-	// cout<<"Element to be deleted "; 
-	// cin>>k;
-	// obj.deletion(k);
-	
-	// // obj.deletion(5);
-	// // obj.print_post_order();
-	// // cout<<endl;
-	// // obj.deletion(7);
-	// cout<<"inorder"<<endl;
-	// obj.print_in_order();
-	// cout<<endl;
-	// cout<<"post order"<<endl;
-	// obj.print_post_order();
-	// // obj.deletion(15);
-	// // obj.deletion(14);
-	// //obj.print_post_order();
-	// //obj.print_post_order() ;
-	// //obj.insertion(25,23);
-	// //cout<<"--";
-	
-	// //obj.print1() ;
 
 	int Q;		//Number of queries.
 	cout<<"--------------------------------------"<<endl;
@@ -91,61 +39,71 @@ int main()
 		cin>>n;
 		switch(n)
 		{
-			case 1  : 
-			cin>>a; obj.put(a, a);
-					break ;
+				case 1  : 
+				cin>>a; 
 
-			case 2  : cin>>a;
-					obj.remove(a);
-					break;
+	obj.put(a, a);
+	break ;
 
-			case 3  : 
-					{
-						cin>>a ;
-					cout<<obj.search(a);
-					break; }
+				case 2  : 
+				cin>>a;
+	obj.remove(a);
+	break;
 
-			case 4  : cout<<obj.maximum()<<endl;
-					break ;
+				case 3  : 
+				cin>>a ;
+	cout<<obj.search(a);
+	break; 
+
+				case 4  : 
+				cout<<obj.maximum()<<endl;
+	break ;
 
 			case 5  : cout<<obj.minimum()<<endl;
-					break ;
+	break ;
 
 			case 6  : 
-				cin>>a; cout<<obj.successor(a)<<endl;
-					break ;
+				cin>>a; 
+	cout<<obj.successor(a)<<endl;
+	break ;
 
-			case 7  : cin>>a;
-					 cout<<obj.predecessor(a)<<endl;
-					break ;
+			case 7  : 
+				cin>>a;
+	cout<<obj.predecessor(a)<<endl;
+	break ;
 
-			case 8  : {cout<<"\n"<<"inorder"<<endl;obj.print_in_order();cout<<endl;
-					break ;}
+			case 8  : 
+			cout<<"\n"<<"inorder"<<endl;
+	obj.print_in_order();cout<<endl;
+	break ;
 
-			case 9  : {cout<<"\n"<<"postorder"<<endl;obj.print_post_order();cout<<endl;
-					break ;}
+			case 9  : cout<<"\n"<<"postorder"<<endl;
+	obj.print_post_order();cout<<endl;
+	break ;
 
 			case 10  :
-			{cout<<"\n"<<"preorder"<<endl ;obj.print_pre_order();cout<<endl;
-					break ;}
+			cout<<"\n"<<"preorder"<<endl ;
+	obj.print_pre_order();cout<<endl;
+	break ;
 
 			case 11  :
-			{ cin>>a ; cout<<obj.get(a);cout<<endl;
-					break ;}
+			 cin>>a ; 
+	cout<<obj.get(a);
+	cout<<endl;
+	break ;
 
 			case 12  :
-			{cout<<obj.size();cout<<endl;
-					break ;}
+			cout<<obj.size();
+	cout<<endl;
+	break ;
 
 			case 13  :
-			{cout<<obj.getheight();cout<<endl;
-					break ;}
+			cout<<obj.getheight();
+	cout<<endl;
+	break ;
 		}
-	 	
-	 	
-	
+	 
 	}
-
 	return 0;
 
 }
--- Trees/RBTree.hpp
+++ Trees/RBTree.hpp
@@ -1,11 +1,7 @@
 #ifndef RBTree_HPP_
 #define RBTree_HPP_ 1
 #include "BSTree.hpp"
-/* The color enumeration.
- * Please use this and not integers or characters to store the color of the node
- * in your red black tree.
- * Also create a class RBTNode which should inherit from BinaryNode and has the attribute color in it. 
- */
+
 
 #define clr(x) (((RBTNode <Key , Value > *)x)->color1)
 #define node_ptr BinaryNode <Key ,Value >*
@@ -37,27 +33,9 @@ class RBTree : public BSTree<Key, Value> {
 protected :
 
 RBTNode <Key , Value > *nil ;
-//node_ptr root  = NULL;
-/* Inherit as many functions as possible from BSTree.
- * Only override those which absolutely need it.
- * Also make sure that all inherited functions work correctly in the context of a red black tree.
- * node_ptr in the following piece of code refers to a pointer to the node you may be using for your red black tree.
- * Use a typedef to refer to a pointer to your node via node_ptr
- */
 
-	/* Function insertRBFixup
-	 *
-	 * Used for:
-	 * Used after an insertion in the rb tree.
-	 * It applies fixing mechanisms to make sure that the tree remains a valid red black tree after an insertion.
-	 */
 	void insertRBFixup(node_ptr root) ;
 
-/* Function deleteRBFixup
-	 * Used for:
-	 * Used after a deletion in the rb tree.
-	 * It applies fixing mechanisms to make sure that the tree remains a valid red black tree after a deletion.
-	 */
 
 	void RBTransplant(node_ptr u, node_ptr v);
 
@@ -120,14 +98,7 @@ RBTNode <Key , Value > *nil ;
 	
 public :
 
-	// using BSTree <Key , Value > :: min ;
-	// using BSTree <Key , Value > :: return_node_of_key ;
-	// using BinaryTree<Key , Value > :: print_in_order ;
-	// using BSTree <Key , Value > :: MINIMUM ;
-	// using BSTree <Key , Value > :: successor ;
-	/* Function : blackHeight
-	 * 
-	 * Returns:
+	 /* Returns:
 	 * the black height of the red black tree which begins at node_ptr root
 	 */
 	RBTree()
